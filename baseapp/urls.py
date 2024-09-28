7b6722f0fa50dc5b231a9821543158fbff76db07
Josh Ouma <oumaj464@gmail.com>
2024-09-28 12:18:33 +0300

    final commit

--- baseapp/urls.py
+++ baseapp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="home"),
+    path('',views.loginpage,name="loginpage"),
+    path('register/',views.register,name="register"),
+    path('home/',views.home,name="home"),
+    path('logout/', views.logout_view, name='logout'),
+    path('Delete/<str:name>/',views.Delete,name="Delete"),
+    path('update/<str:name>/',views.update,name="update"),
 ]
